78ceecf15c51249e5ce179e99090724280d7d077
Kevin A. Naudé <1943178+kevin-in-code@users.noreply.github.com>
2024-07-01 00:05:38 +1000

    Re-export components and definitions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,33 @@
-// Reexport your entry components here
+import DefaultIcon from '$lib/default-icon.svelte';
+import DefaultTitle from '$lib/default-title.svelte';
+
+import DashedToast from '$lib/designs/dashed-toast.svelte';
+import FramedToast from '$lib/designs/framed-toast.svelte';
+import SkewedToast from '$lib/designs/skewed-toast.svelte';
+import WingedToast from '$lib/designs/winged-toast.svelte';
+
+import CloseIcon from '$lib/icons/close-icon.svelte';
+import ErrorIcon from '$lib/icons/error-icon.svelte';
+import InformationIcon from '$lib/icons/information-icon.svelte';
+import SuccessIcon from '$lib/icons/success-icon.svelte';
+import WarningIcon from '$lib/icons/warning-icon.svelte';
+
+import ToastView from '$lib/toast-view.svelte';
+
+export * from '$lib/context.js';
+export * from '$lib/themes.js';
+
+export {
+  CloseIcon,
+  DashedToast,
+  DefaultIcon,
+  DefaultTitle,
+  ErrorIcon,
+  FramedToast,
+  InformationIcon,
+  SkewedToast,
+  SuccessIcon,
+  ToastView,
+  WarningIcon,
+  WingedToast,
+};
