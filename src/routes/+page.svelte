b30eb44805d507fc3d295800eb5cfbd7488233d1
Kevin A. Naudé <1943178+kevin-in-code@users.noreply.github.com>
2024-07-01 00:08:03 +1000

    Update demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,179 +1,175 @@
 <script lang="ts">
-  import type { ToastContext } from '$lib/context.js';
-  import { marker as theme } from '$lib/themes.js';
-  import ToastContextProvider from '$lib/toast-context-provider.svelte';
-  import ToastView from '$lib/toast-view.svelte';
-  let context: ToastContext;
+  import { tick } from 'svelte';
 
-  let categoryOption: 'Success' | 'Information' | 'Warning' | 'Error' = 'Information';
-  let customTitleOption: 'None' | 'Default' | 'Custom' = 'Default';
+  import { ToastView, action, candy, clearToasts, crisp, marker, raiseToast } from '$lib/index.js';
 
-  $: {
-    if (context) {
-      context.raise({
-        category: 'error',
-        topic: 'Your app status is buried in console.log!',
-        body: 'You could surface the information in a toast.',
-        duration: 5000,
-      });
-      context.raise({
-        category: 'warning',
-        topic: 'Creating toast functionality from scratch may shift your focus',
-        duration: 5000,
-      });
-      context.raise({
-        category: 'information',
-        topic: 'Good toast libraries already exist',
+  import SaveNow from './save-now.svelte';
+
+  let theme = marker;
+  let mode: 'light' | 'dark' = 'light';
+  let numberOfToastsRaised = 0;
+  let abortDemo: AbortController | undefined;
+
+  function delay(milliseconds: number): Promise<void> {
+    return new Promise<void>((resolve) => setTimeout(resolve, milliseconds));
+  }
+
+  const toastFactories = [
+    async (animate: boolean) => {
+      const control = raiseToast({
+        topic: 'Out-of-band events happen',
         body: [
-          'Consider using this one.',
-          "It has some nice features. For example, it's themes are nice, and they work well on both light and dark pages.",
+          "Out-of-band events in your app will happen, and you may need to bring them to the user's attention.",
+          'Toasts allow you to do this without taking the user out of the current activity.',
         ],
-        duration: 5000,
+        status: {
+          collapsed: 'clicking a toast may reveal more',
+          expanded: 'toasts are keyboard-friendly',
+        },
       });
-      context.raise({
+      if (animate) {
+        await delay(1500);
+        control.expand(true);
+        await delay(4500);
+        control.expand(false);
+        await delay(500);
+      }
+    },
+    async (animate: boolean) => {
+      const control = raiseToast({
+        category: 'warning',
+        topic: 'You have made 39 changes to this document without saving.',
+        body: SaveNow,
+        status: { expanded: 'a toast body can contain any component', collapsed: '' },
+      });
+      if (animate) {
+        await delay(2500);
+        control.expand(true);
+        await delay(4500);
+        control.expand(false);
+        await delay(500);
+      }
+    },
+    async (animate: boolean) => {
+      const control = raiseToast({
+        category: 'information',
+        topic: 'Uploading your video...',
+        clipTitleOnCollapse: true,
+        showStatusInlineWhenCollapsed: true,
+      });
+      control.update({ taskProgress: 0, taskScale: 20 });
+
+      let progress = 0;
+      const completion = new Promise<void>((resolve) => {
+        const timer = setInterval(() => {
+          if (progress < 20) {
+            const extra = Math.floor(Math.random() * 4);
+            progress = Math.min(20, progress + extra);
+            control.update({ taskProgress: progress });
+            if (progress === 20) {
+              control.update({ status: 'Done' });
+              clearInterval(timer);
+              resolve();
+            }
+          }
+        }, 500);
+      });
+      if (animate) {
+        await completion;
+        await delay(2000);
+      }
+    },
+    async (animate: boolean) => {
+      const control = raiseToast({
+        category: 'error',
+        topic: 'A network error has occurred',
+        body: 'This toast demonstrates auto-expiry of toasts.',
+        status: { expanded: 'interacting with the toast prevents expiry', collapsed: '' },
+        showExpiryCountdown: true,
+        expiresIn: 5000,
+      });
+      if (animate) {
+        await delay(3000);
+        control.expand(true);
+        await delay(3500);
+        control.expand(false);
+        await delay(500);
+      }
+    },
+    async (animate: boolean) => {
+      const control = raiseToast({
         category: 'success',
-        topic: 'Your app status is now clear to your users',
-        duration: 5000,
+        topic: 'Achievement Unlocked!',
+        body: [
+          'Toasts now allow you to provide essential information to your users without breaking their flow.',
+        ],
+        status: { expanded: '✔️', collapsed: '' },
       });
+      if (animate) {
+        await delay(1500);
+        control.expand(true);
+        await delay(4500);
+        control.expand(false);
+        await delay(500);
+      }
+    },
+  ];
+
+  function reset() {
+    clearToasts();
+    numberOfToastsRaised = 0;
+    abortDemo?.abort();
+    abortDemo = undefined;
+  }
+
+  async function post(animate: boolean = false): Promise<void> {
+    const toastToRaise = toastFactories[numberOfToastsRaised++ % toastFactories.length];
+    await toastToRaise(animate);
+  }
+
+  async function demo() {
+    reset();
+    await tick();
+    let localAbortDemo = new AbortController();
+    abortDemo = localAbortDemo;
+    clearToasts();
+    for (let k = 0; k < toastFactories.length; k++) {
+      if (localAbortDemo.signal.aborted) return;
+      await post(true);
+    }
+    if (abortDemo === localAbortDemo) {
+      abortDemo = undefined;
     }
   }
+
+  setTimeout(post, 0);
 </script>
 
-<ToastContextProvider bind:context defaultPosition="center" {theme}>
-  <div class="panel" class:dark={false}>
-    <div>
-      <ToastView />
+<ToastView {theme} zone="top" />
+<div class="panel" class:dark={mode === 'dark'}>
+  <div></div>
+  <div class="controls">
+    <div role="group" class="row">
+      <button class:outline={theme !== action} on:click={() => (theme = action)}>action</button>
+      <button class:outline={theme !== marker} on:click={() => (theme = marker)}>marker</button>
+      <button class:outline={theme !== candy} on:click={() => (theme = candy)}>candy</button>
+      <button class:outline={theme !== crisp} on:click={() => (theme = crisp)}>crisp</button>
     </div>
-    <!-- <div>
-    <ToastView />
-  </div> -->
-
-    <!-- <div class="controls">
-  <fieldset>
-    <legend>Category</legend>
-
-    <label>
-      <input type="radio" id="success-category" name="category" value="success" checked />
-      Success
-    </label>
-
-    <label>
-      <input type="radio" id="info-category" name="category" value="info" />
-      Information
-    </label>
-
-    <label>
-      <input type="radio" id="warning-category" name="category" value="warning" />
-      Warning
-    </label>
-
-    <label>
-      <input type="radio" id="error-category" name="category" value="error" />
-      Error
-    </label>
-  </fieldset>
-
-  <fieldset>
-    <legend>Title</legend>
-
-    <label>
-      <input type="radio" id="title-none" name="title" value="None" bind:group={customTitleOption} />
-      None
-    </label>
-
-    <label>
-      <input type="radio" id="title-default" name="title" value="Default" bind:group={customTitleOption} />
-      Default
-    </label>
-
-    <label>
-      <input type="radio" id="title-custom" name="title" value="Custom" bind:group={customTitleOption} />
-      Custom
-    </label>
-
-    <div>
-      <input type="text" id="title-custom-text" placeholder="Custom title text..." disabled={customTitleOption !== 'Custom'} />
+    <div role="group" class="row">
+      <button class:outline={mode !== 'light'} on:click={() => (mode = 'light')}
+        >on light background</button
+      >
+      <button class:outline={mode !== 'dark'} on:click={() => (mode = 'dark')}
+        >on dark background</button
+      >
     </div>
-  </fieldset>
-</div> -->
-
-    <div class="controls">
-      <!-- <fieldset role="group">
-    <legend>Category</legend> -->
-      <div role="group" class="row">
-        <button
-          class:outline={categoryOption !== 'Success'}
-          on:click={() => (categoryOption = 'Success')}>Success</button
-        >
-        <button
-          class:outline={categoryOption !== 'Information'}
-          on:click={() => (categoryOption = 'Information')}>Information</button
-        >
-        <button
-          class:outline={categoryOption !== 'Warning'}
-          on:click={() => (categoryOption = 'Warning')}>Warning</button
-        >
-        <button
-          class:outline={categoryOption !== 'Error'}
-          on:click={() => (categoryOption = 'Error')}>Error</button
-        >
-        <label><input type="checkbox" />abc</label>
-        <!-- <label for="success-category">
-        <input type="radio" id="success-category" name="category" value="success" checked />
-        Success
-      </label>
-
-      <input type="radio" id="info-category" name="category" value="info" />
-      <label for="info-category">Information</label>
-
-      <input type="radio" id="warning-category" name="category" value="warning" />
-      <label for="warning-category">Warning</label>
-
-      <input type="radio" id="error-category" name="category" value="error" />
-      <label for="error-category">Error</label> -->
-      </div>
-      <!-- </fieldset> -->
-      <fieldset>
-        <legend>Title</legend>
-        <div class="row">
-          <input
-            type="radio"
-            id="title-none"
-            name="title"
-            value="None"
-            bind:group={customTitleOption}
-          />
-          <label for="title-none">None</label>
-
-          <input
-            type="radio"
-            id="title-default"
-            name="title"
-            value="Default"
-            bind:group={customTitleOption}
-          />
-          <label for="title-default">Default</label>
-
-          <input
-            type="radio"
-            id="title-custom"
-            name="title"
-            value="Custom"
-            bind:group={customTitleOption}
-          />
-          <label for="title-custom">Custom</label>
-
-          <input
-            type="text"
-            id="title-custom-text"
-            placeholder="Custom title text..."
-            disabled={customTitleOption !== 'Custom'}
-          />
-        </div>
-      </fieldset>
+    <div class="row gap">
+      <button class="contrast" on:click={demo}>Run a Demo</button>
+      <button on:click={reset}>Clear</button>
+      <button on:click={() => post()}>Raise a Toast</button>
     </div>
   </div>
-</ToastContextProvider>
+</div>
 
 <style>
   :global(:root) {
@@ -187,58 +183,27 @@
     align-items: stretch;
   }
 
-  .panel > * {
+  .panel > :first-child {
     flex: 1;
-    min-height: 600px;
     display: flex;
+  }
+
+  .controls {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1em;
+    padding: 1em;
+    align-items: baseline;
+  }
+
+  .controls > * {
+    flex: 1 1 content;
+    margin-bottom: 0;
   }
 
   .dark {
     background-color: rgb(36, 60, 66);
   }
-
-  fieldset {
-    border: 0.0625em solid #1f497d;
-    background: #ddd;
-    border-radius: 0.25em;
-    padding: 1em;
-    margin: 1em;
-    width: unset;
-    min-height: initial;
-    flex: initial;
-    display: initial;
-  }
-
-  fieldset > legend {
-    background: #1f497d;
-    color: #fff;
-    padding: 0.125em 0.5em;
-    font-size: 1.5em;
-    border-radius: 0.167em;
-    box-shadow: 0 0 0 0.167em #ddd;
-    margin-left: 0.25em;
-  }
-
-  .row > * + input {
-    margin-left: 0.5em;
-  }
-
-  input[type='text'] {
-    flex: 1 1 5em;
-  }
-
-  /* .controls {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    flex-direction: row;
-    justify-content: center;
-  }
-
-  .controls > * {
-    width: auto;
-    padding: 1em;
-  } */
 
   .row {
     display: flex;
@@ -246,10 +211,15 @@
   }
 
   .row > * {
-    flex: 0 0 auto;
+    flex: 1 1 content;
   }
 
-  fieldset {
-    margin: 1em;
+  .gap {
+    gap: 1em;
+  }
+
+  button {
+    --pico-form-element-spacing-horizontal: 0.5em;
+    --pico-form-element-spacing-vertical: 0.5em;
   }
 </style>
---
+++ src/routes/save-now.svelte
@@ -0,0 +1,10 @@
+<p>Save changes now? <button>Save</button></p>
+
+<style>
+  p {
+    color: inherit;
+  }
+  button {
+    padding: 0.125em 0.25em;
+  }
+</style>
